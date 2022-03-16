f8f3d22d7403369b067fc73af7d99038843c459a
mahmudul1995 <mahmuduls451@gmail.com>
2022-03-16 19:59:45 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function updateCaseNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + "-number");
+    let productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    //Update Case Total
+
+    const productTotal = document.getElementById(product + "-total");
+    productTotal.innerText = productNumber * price;
+    // calculateTotal
+
+    calculateTotal();
+
+}
+
